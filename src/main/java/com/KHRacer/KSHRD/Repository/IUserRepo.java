1b9b575c417fc70d9a213e18ba1fabd1561251ba
Heng Borchhay <hborchay@gmail.com>
2020-07-06 21:33:07 +0700

    User temp hold error

--- src/main/java/com/KHRacer/KSHRD/Repository/IUserRepo.java
+++ src/main/java/com/KHRacer/KSHRD/Repository/IUserRepo.java
@@ -3,7 +3,9 @@ package com.KHRacer.KSHRD.Repository;
 import com.KHRacer.KSHRD.Model.UserModel;
 import com.KHRacer.KSHRD.Repository.Provider.UserProvider;
 import org.apache.ibatis.annotations.SelectProvider;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IUserRepo {
     @SelectProvider(value= UserProvider.class,method = "getUserByIDSQL")
     public UserModel getUserByID(int id);
--- src/main/java/com/KHRacer/KSHRD/Repository/Provider/UserProvider.java
+++ src/main/java/com/KHRacer/KSHRD/Repository/Provider/UserProvider.java
@@ -1,4 +1,13 @@
 package com.KHRacer.KSHRD.Repository.Provider;
 
+import org.apache.ibatis.jdbc.SQL;
+
 public class UserProvider {
+    public String getUserByIDSQL(){
+        return new SQL(){{
+            SELECT("*");
+            FROM("finalproject.user");
+            WHERE("user_id=#{id}");
+        }}.toString();
+    }
 }
